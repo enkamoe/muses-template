072d4cee15f51e734c5fe1a0d5cea2be4b6d0f80
icedlatte24 <166580644+icedlatte24@users.noreply.github.com>
2024-07-13 23:33:37 +0900

    続き

--- js-template/consul&apply.js
+++ js-template/consul&apply.js
@@ -1,9 +1,13 @@
 "use strict";
 
 function confirmCancel() {
-  if (confirm("本当に取消をしますか？") == true) {
+  if (confirm("本当に面談を取消ししますか？") == true) {
     document.getElementById("message").innerText =
       "現在の面談予定はありません。";
   }
   return false;
 }
+
+document.getElementById('confirmButton').addEventListener('click', function( {
+  const 
+}))
