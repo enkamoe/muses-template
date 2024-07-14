6ec14b20966dbfa05eb37afe8892171b3e85ac4e
icedlatte24 <166580644+icedlatte24@users.noreply.github.com>
2024-07-14 11:30:11 +0900

    続き

--- js-template/consul&apply.js
+++ js-template/consul&apply.js
@@ -1,5 +1,27 @@
 "use strict";
 
+document.getElementById("confirmButton").addEventListener("click", function () {
+  const consultType = document.querySelector('input[name="consult"]:checked');
+  const consultDate = document.getElementById("consultdate").value;
+  const message = document.querySelector("textarea").value;
+
+  if (!consultType) {
+    alert("相談種別を選択してください。");
+    return;
+  }
+
+  if (!consultDate) {
+    alert("相談日程を選択してください。");
+    return;
+  }
+
+  let confirmApply = `相談種別：${consultType.parentNode.textContent.trim()}\n`;
+  confirmApply += `相談日程：${consultDate}\n`;
+  confirmApply += `伝えておきたい事：${message ? message : "特になし"}`;
+
+  alert(confirmApply);
+});
+
 function confirmCancel() {
   if (confirm("本当に面談を取消ししますか？") == true) {
     document.getElementById("message").innerText =
@@ -7,7 +29,3 @@ function confirmCancel() {
   }
   return false;
 }
-
-document.getElementById('confirmButton').addEventListener('click', function( {
-  const 
-}))
